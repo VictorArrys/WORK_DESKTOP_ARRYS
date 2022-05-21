4010618ce1394c8651dfa73e87a5753f45d4fa1d
AbrahamCRV <danielcrv7@outlook.com>
2022-05-21 13:35:08 -0500

    Se agregaron pantallas de categorias y perfil administrador

---
+++ Cliente/El_Camello/Vistas/Administrador/categoriasEmpleo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace El_Camello.Vistas.Administrador
+{
+    /// <summary>
+    /// Lógica de interacción para categoriasEmpleo.xaml
+    /// </summary>
+    public partial class categoriasEmpleo : Window
+    {
+        public categoriasEmpleo()
+        {
+            InitializeComponent();
+        }
+    }
+}
