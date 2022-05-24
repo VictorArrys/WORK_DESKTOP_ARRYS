b7067ebb5bfffa018f752adc4ad6bad8abb77e82
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-05-24 09:15:07 -0500

    Se agrego el idTipoUsuario al token y al cuerpo de las respuesta del endpoint /iniciarSesion

--- API/service/AccesoAlSistemaService.js
+++ API/service/AccesoAlSistemaService.js
@@ -2,7 +2,6 @@
 const jwt = require('jsonwebtoken');
 var mysqlConnection = require('../utils/conexion');
 const keys = require('../settings/keys');
-const { response } = require('express');
 
 /**
  * Cierra sesión del usuario y desecha el token
@@ -39,7 +38,7 @@ exports.cerrarSesion = function() {
  **/
 exports.iniciarSesion = function(nombreUsuario,clave) {
   return new Promise(function(resolve, reject) {
-    var query = 'SELECT * FROM perfil_usuario WHERE nombre_usuario = ? AND clave = ?;'
+    var query = 'SELECT U.*, A.id_perfil_usuario_admin as id_administrador, B.id_perfil_usuario_aspirante as id_aspirante, C.id_perfil_usuario_empleador as id_empleador, D.id_perfil_usuario_demandante AS id_demandante FROM perfil_usuario As U left join perfil_administrador AS A ON U.id_perfil_usuario = A.id_perfil_usuario_admin left join perfil_aspirante AS B ON U.id_perfil_usuario = B.id_perfil_usuario_aspirante left join perfil_empleador AS C ON U.id_perfil_usuario = C.id_perfil_usuario_empleador left join perfil_demandante AS D ON U.id_perfil_usuario = D.id_perfil_usuario_demandante WHERE nombre_usuario = ? AND clave = ?;'
     console.log('Conectado con el identificador ' + mysqlConnection.threadId);
     
     mysqlConnection.query(query, [nombreUsuario, clave], function (error, results, fields){
@@ -71,7 +70,32 @@ exports.iniciarSesion = function(nombreUsuario,clave) {
           const token = jwt.sign(payload, keys.key, {
             expiresIn: 60 * 60 * 24
           });
-
+          var idTipoUsuario = null;
+          switch(usuario['tipo_usuario']) {
+            case "Administrador":
+              idTipoUsuario = usuario['id_administrador'];
+              break;
+            case "Empleador":
+              idTipoUsuario = usuario['id_empleador'];
+              break;
+            case "Demandante":
+              idTipoUsuario = usuario['id_demandante'];
+              break;
+            case "Aspirante":
+              idTipoUsuario = usuario['id_aspirante'];
+              break;
+          }
+          console.log(idTipoUsuario);
+          
+          //Si el usuario no tiene un perfil de admin, empleoador, aspirante o demandante no podra inciar sesión.
+          if (idTipoUsuario == null) {
+            reject({
+              "resBody" : {
+                "menssage" : "Usuario no disponible", 
+              }, 
+              "statusCode" : 404
+            });
+          }
 
           const resultadoRes = {};
           resultadoRes['application/json'] = {
@@ -83,6 +107,7 @@ exports.iniciarSesion = function(nombreUsuario,clave) {
               "correoElectronico" : usuario['correo_electronico'],
               "fotografia" : usuario['fotografia'],
               "tipoUsuario" : usuario['tipo_usuario'],
+              "idTipoUsuario" : idTipoUsuario,
               "token" : token
             },
             "statusCode" : 200
