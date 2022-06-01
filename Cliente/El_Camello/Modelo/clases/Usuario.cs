3c3fed9d2334454bb6a64e224eed1469ecd02b7e
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-06-01 10:50:34 -0500

    Login y Menu de Administrador agregados
    Las clases Usuario y UsuarioDAO son utilizadas para la funcion de incio de sesión.

---
+++ Cliente/El_Camello/Modelo/clases/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_Camello.Modelo.clases
+{
+    public class Usuario
+    {
+        private string clave;
+        private string tipo;
+        private int estatus;
+        private int idPerfilusuario;
+        private string correoElectronico;
+        private byte[] fotografia;
+        private string tipoUsuario;
+        private string token;
+
+        public string Clave { get => clave; set => clave = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public int Estatus { get => estatus; set => estatus = value; }
+        public int IdPerfilusuario { get => idPerfilusuario; set => idPerfilusuario = value; }
+        public string CorreoElectronico { get => correoElectronico; set => correoElectronico = value; }
+        public byte[] Fotografia { get => fotografia; set => fotografia = value; }
+        public string TipoUsuario { get => tipoUsuario; set => tipoUsuario = value; }
+        public string Token { get => token; set => token = value; }
+
+        public override string ToString()
+        {
+            return clave + "\n" + tipo + "\n" + estatus + "\n" + fotografia;
+        }
+    }
+}
---
+++ Cliente/El_Camello/Vistas/Usuario/Login.xaml.cs
@@ -0,0 +1,59 @@
+﻿using El_Camello.Modelo.dao;
+using El_Camello.Modelo.clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using El_Camello.Vistas.Administrador;
+
+namespace El_Camello.Vistas.Usuario
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIniciarSesion_Click(object sender, RoutedEventArgs e)
+        {
+            string nombreUsuario = txtNombreUsuario.Text;
+            string clave = pwdClave.Password;
+            Modelo.clases.Usuario usuario = await UsuarioDAO.iniciarSesion(nombreUsuario, clave);
+            if (usuario != null)
+            {
+                switch (usuario.Tipo)
+                {
+                    case "Administrador":
+                        MenuAdministrador menu = new MenuAdministrador(usuario);
+                        menu.Show();
+                        this.Close();
+                        break;
+                    case "Aspirante":
+                        break;
+                    case "Demandante":
+                        break;
+                    case "Empleador":
+                        break;
+                }
+            }
+        }
+
+        private void btnRestablcerClave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
