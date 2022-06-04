2ed903e4102c8de9ddad2b520b09303d4a1001d3
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-06-04 16:59:49 -0500

    Se añaden nombre de variable a RegistrarAspirante.xaml y se crea la clase Oficio

---
+++ Cliente/El_Camello/Modelo/clases/Oficio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_Camello.Modelo.clases
+{
+    internal class Oficio
+    {
+        private int idAspirante;
+        private int idCategoria;
+        private string experiencia;
+        private string nombreCategoria;
+    }
+}
