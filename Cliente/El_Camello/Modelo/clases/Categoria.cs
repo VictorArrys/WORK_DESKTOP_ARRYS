4ffb6bc4acee2ed4a922a6376480c760302c4ad1
AbrahamCRV <danielcrv7@outlook.com>
2022-06-04 13:15:31 -0500

    Se agrega funcionalida de categorias en el cliente desktop
    CRUD de categoria de empleo funcional

---
+++ Cliente/El_Camello/Modelo/clases/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_Camello.Modelo.clases
+{
+    internal class Categoria
+    {
+        private int idCategoria;
+        private string nombreCategoria;
+
+        public int IdCategoria { get => idCategoria; set => idCategoria = value; }
+        public string NombreCategoria { get => nombreCategoria; set => nombreCategoria = value; }
+
+
+    }
+}
